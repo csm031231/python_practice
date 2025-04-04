d39af2b32b93d2faba6992ca967a5a41a316e009
csm031231 <csumim@naver.com>
2025-04-04 23:34:11 +0900

    04.04

--- python 기초 100제/6009~6024.py
+++ python 기초 100제/6009~6024.py
@@ -1,22 +1,22 @@
-# #6009
-# a = input()
-# print(a)
+#6009
+a = input()
+print(a)
 
-# #6010
-# a = input()
-# a = int(a)
-# print(a)
+#6010
+a = input()
+a = int(a)
+print(a)
 
-# #6011
-# a = input()
-# a = float(a)
-# print(a)
+#6011
+a = input()
+a = float(a)
+print(a)
 
-# #6012
-# a = int(input())
-# b = int(input())
-# print(a)
-# print(b)
+#6012
+a = int(input())
+b = int(input())
+print(a)
+print(b)
 
 #6013
 a = input()
